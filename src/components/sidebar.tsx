b9f7b4a65a34d4765d41aaf415412b254eba9d5a
Rhino-ty <okko8522@gmail.com>
2023-06-13 17:06:37 +0900

    design: 사이드바 1번째 아이콘 디자인

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -9,60 +9,69 @@ import Link from "next/link";
 
 export default function Sidebar() {
   return (
-    <div className="flex flex-row h-full fixed">
-      <nav className="flex flex-col justify-between w-20 h-screen bg-white dark:bg-gray-800">
-        <div className="mt-10 mb-10">
-          <ul>
-            <li className="my-12 text-center">
-              <Link href="/">
-                <HomeRoundedIcon className="h-7 w-7 text-gray-500 dark:text-gray-300 mx-auto hover:text-gray-800 dark:hover:text-white transition-colors duration-200" />
-              </Link>
-            </li>
-            <li className="my-12 text-center">
-              <Link href="/get-started">
-                <AppsRoundedIcon className="h-7 w-7 text-gray-500 dark:text-gray-300 mx-auto hover:text-gray-800 dark:hover:text-white transition-colors duration-200" />
-              </Link>
-            </li>
-            <li className="my-12 text-center">
-              <Link href="/develop">
-                <CodeIcon className="h-7 w-7 text-gray-500 dark:text-gray-300 mx-auto hover:text-gray-800 dark:hover:text-white transition-colors duration-200" />
-              </Link>
-            </li>
-            <li className="my-12 text-center">
-              <Link href="/foundations">
-                <BookOutlinedIcon className="h-7 w-7 text-gray-500 dark:text-gray-300 mx-auto hover:text-gray-800 dark:hover:text-white transition-colors duration-200" />
-              </Link>
-            </li>
-            <li className="my-12 text-center">
-              <Link href="styles">
-                <PaletteOutlinedIcon className="h-7 w-7 text-gray-500 dark:text-gray-300 mx-auto hover:text-gray-800 dark:hover:text-white transition-colors duration-200" />
-              </Link>
-            </li>
-          </ul>
-        </div>
-
-        <div className="mb-4">
-          <a href="#">
-            <span>
-              <svg
-                className="w-5 h-5 mx-auto text-gray-300 fill-current hover:text-red-500"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
+    <nav className="flex flex-col justify-between h-screen bg-white dark:bg-gray-800 fixed">
+      <div className="flex flex-col w-20 mt-5">
+        <ul>
+          <li>
+            <div className="flex justify-center">
+              <Link
+                href="/"
+                className="w-20 mx-auto -mt-1 mb-4 p-1 text-center"
               >
-                <path
-                  d="M13 4.00894C13.0002 3.45665 12.5527 3.00876 12.0004 3.00854C11.4481 3.00833 11.0002 3.45587 11 4.00815L10.9968 12.0116C10.9966 12.5639 11.4442 13.0118 11.9965 13.012C12.5487 13.0122 12.9966 12.5647 12.9968 12.0124L13 4.00894Z"
-                  fill="currentColor"
-                ></path>
-                <path
-                  d="M4 12.9917C4 10.7826 4.89541 8.7826 6.34308 7.33488L7.7573 8.7491C6.67155 9.83488 6 11.3349 6 12.9917C6 16.3054 8.68629 18.9917 12 18.9917C15.3137 18.9917 18 16.3054 18 12.9917C18 11.3348 17.3284 9.83482 16.2426 8.74903L17.6568 7.33481C19.1046 8.78253 20 10.7825 20 12.9917C20 17.41 16.4183 20.9917 12 20.9917C7.58172 20.9917 4 17.41 4 12.9917Z"
-                  fill="currentColor"
-                ></path>
-              </svg>
-            </span>
-          </a>
-        </div>
-      </nav>
-    </div>
+                {/* 다크 적용 */}
+                {/* <HomeRoundedIcon className="dark:text-gray-300 dark:hover:text-white" />
+          <label className="dark:text-gray-800 dark:hover:text-white"> */}
+                <HomeRoundedIcon className="w-14 h-8 text-gray-300 hover:text-gray-100 transition-colors" />
+                <label className="text-xs cursor-pointer text-gray-300 relative items-center justify-center hover:text-gray-100 transition-colors">
+                  Home
+                </label>
+              </Link>
+            </div>
+          </li>
+          <li className="text-center">
+            <Link href="/get-started">
+              <AppsRoundedIcon className="h-7 w-7 text-gray-500 dark:text-gray-300 mx-auto hover:text-gray-800 dark:hover:text-white transition-colors" />
+            </Link>
+          </li>
+          <li className="text-center">
+            <Link href="/develop">
+              <CodeIcon className="h-7 w-7 text-gray-500 dark:text-gray-300 mx-auto hover:text-gray-800 dark:hover:text-white transition-colors" />
+            </Link>
+          </li>
+          <li className="text-center">
+            <Link href="/foundations">
+              <BookOutlinedIcon className="h-7 w-7 text-gray-500 dark:text-gray-300 mx-auto hover:text-gray-800 dark:hover:text-white transition-colors" />
+            </Link>
+          </li>
+          <li className="text-center">
+            <Link href="styles">
+              <PaletteOutlinedIcon className="h-7 w-7 text-gray-500 dark:text-gray-300 mx-auto hover:text-gray-800 dark:hover:text-white transition-colors" />
+            </Link>
+          </li>
+        </ul>
+      </div>
+
+      <div className="mb-4">
+        <a href="#">
+          <span>
+            <svg
+              className="w-5 h-5 mx-auto text-gray-300 fill-current hover:text-red-500"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M13 4.00894C13.0002 3.45665 12.5527 3.00876 12.0004 3.00854C11.4481 3.00833 11.0002 3.45587 11 4.00815L10.9968 12.0116C10.9966 12.5639 11.4442 13.0118 11.9965 13.012C12.5487 13.0122 12.9966 12.5647 12.9968 12.0124L13 4.00894Z"
+                fill="currentColor"
+              ></path>
+              <path
+                d="M4 12.9917C4 10.7826 4.89541 8.7826 6.34308 7.33488L7.7573 8.7491C6.67155 9.83488 6 11.3349 6 12.9917C6 16.3054 8.68629 18.9917 12 18.9917C15.3137 18.9917 18 16.3054 18 12.9917C18 11.3348 17.3284 9.83482 16.2426 8.74903L17.6568 7.33481C19.1046 8.78253 20 10.7825 20 12.9917C20 17.41 16.4183 20.9917 12 20.9917C7.58172 20.9917 4 17.41 4 12.9917Z"
+                fill="currentColor"
+              ></path>
+            </svg>
+          </span>
+        </a>
+      </div>
+    </nav>
   );
 }
