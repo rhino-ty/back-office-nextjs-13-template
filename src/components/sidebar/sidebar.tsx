8dbaf4536dc56eb5f2c62fc4c5426ded1b8e9684
Rhino-ty <okko8522@gmail.com>
2023-06-13 18:41:00 +0900

    Design: 다크모드 관련 디자인 수정

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -11,56 +11,46 @@ import "./sidebar.css";
 
 export default function Sidebar() {
   return (
-    <nav className="flex flex-col justify-between h-screen bg-white dark:bg-gray-800 fixed">
+    <nav className="flex flex-col justify-between h-screen bg-gray-200 dark:bg-gray-800 fixed">
       <div className="flex flex-col w-20 mt-5">
         <ul>
           <li>
             <div className="flex justify-center">
               <Link href="/" className="sidebar-link group">
-                <HomeRoundedIcon className="icon-style group-hover:bg-opacity-60 group-hover:scale-105" />
-                <label className="label-style group-hover:text-gray-100 ">
-                  Home
-                </label>
+                <HomeRoundedIcon className="icon-style group-hover:bg-opacity-70 group-hover:scale-105" />
+                <label className="label-style ">Home</label>
               </Link>
             </div>
           </li>
           <li>
             <div className="flex justify-center">
               <Link href="/get-started" className="sidebar-link group">
-                <AppsRoundedIcon className="icon-style group-hover:bg-opacity-60 group-hover:scale-105" />
-                <label className="label-style group-hover:text-gray-100 ">
-                  Home
-                </label>
+                <AppsRoundedIcon className="icon-style group-hover:bg-opacity-70 group-hover:scale-105" />
+                <label className="label-style ">Home</label>
               </Link>
             </div>
           </li>
           <li>
             <div className="flex justify-center">
               <Link href="/develop" className="sidebar-link group">
-                <CodeIcon className="icon-style group-hover:bg-opacity-60 group-hover:scale-105" />
-                <label className="label-style group-hover:text-gray-100 ">
-                  Develop
-                </label>
+                <CodeIcon className="icon-style group-hover:bg-opacity-70 group-hover:scale-105" />
+                <label className="label-style ">Develop</label>
               </Link>
             </div>
           </li>
           <li>
             <div className="flex justify-center">
               <Link href="/foundations" className="sidebar-link group">
-                <BookOutlinedIcon className="icon-style group-hover:bg-opacity-60 group-hover:scale-105" />
-                <label className="label-style group-hover:text-gray-100 ">
-                  Foundations
-                </label>
+                <BookOutlinedIcon className="icon-style group-hover:bg-opacity-70 group-hover:scale-105" />
+                <label className="label-style">Foundations</label>
               </Link>
             </div>
           </li>
           <li>
             <div className="flex justify-center">
               <Link href="/styles" className="sidebar-link group">
-                <PaletteOutlinedIcon className="icon-style group-hover:bg-opacity-60 group-hover:scale-105" />
-                <label className="label-style group-hover:text-gray-100 ">
-                  Styles
-                </label>
+                <PaletteOutlinedIcon className="icon-style group-hover:bg-opacity-70 group-hover:scale-105" />
+                <label className="label-style">Styles</label>
               </Link>
             </div>
           </li>
