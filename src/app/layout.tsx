29397753bdcf140fbb2554211fc5ec052ab7ace9
Rhino-ty <okko8522@gmail.com>
2023-06-13 15:13:11 +0900

    src/app 디렉토리 관련 초기 세팅
    사용할 URL Path 관련 폴더 생성 및 기본 재료 page.js 생성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import { Inter } from 'next/font/google';
+import './style/globals.css';
 
 const inter = Inter({ subsets: ['latin'] })
 
