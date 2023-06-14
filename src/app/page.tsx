56c93125cbccefdee4263d60d1cebf7ee44bddbf
Rhino-ty <okko8522@gmail.com>
2023-06-14 10:32:59 +0900

    design: 글 색상 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-row items-center justify-center">
       Hi! This is
-      <Link href="https://m3.material.io/" className="ml-1">
+      <Link href="https://m3.material.io/" className="ml-1 hover:text-blue-400">
         Home
       </Link>
       !
