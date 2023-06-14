b908a040278e4865f08a08de827e83bc015b363a
Rhino-ty <okko8522@gmail.com>
2023-06-14 11:16:45 +0900

    feat: drawer 메뉴 구현
    isDrawerOpen 상태를 사용하여 onMouseEnter, onMouseLeave를 각각 조작하게 만듦

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -6,16 +6,30 @@ import CodeIcon from "@mui/icons-material/Code";
 import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
 import PaletteOutlinedIcon from "@mui/icons-material/PaletteOutlined";
 import Link from "next/link";
+import { useState } from "react";
 import DarkModeToggleButton from "../darkModeToggleButton";
 import "./sidebar.css";
 
 export default function Sidebar() {
+  const [isDrawerOpen, setDrawerOpen] = useState(false);
+
+  const handleDrawerMenuHover = () => {
+    setDrawerOpen(true);
+  };
+
+  const handleDrawerMenuClose = () => {
+    setDrawerOpen(false);
+  };
+
   return (
     <nav className="flex flex-col justify-between h-screen bg-gray-200 dark:bg-gray-800 fixed">
       <div className="flex flex-col w-[88px] mt-5">
         <ul>
           <li>
-            <div className="flex justify-center">
+            <div
+              className="flex justify-center"
+              onMouseEnter={handleDrawerMenuClose}
+            >
               <Link href="/" className="sidebar-link group">
                 <HomeRoundedIcon className="icon-style group-hover:bg-opacity-70 group-hover:scale-105" />
                 <label className="label-style ">Home</label>
@@ -23,7 +37,10 @@ export default function Sidebar() {
             </div>
           </li>
           <li>
-            <div className="flex justify-center">
+            <div
+              className="flex justify-center"
+              onMouseEnter={handleDrawerMenuClose}
+            >
               <Link href="/get-started" className="sidebar-link group">
                 <AppsRoundedIcon className="icon-style group-hover:bg-opacity-70 group-hover:scale-105" />
                 <label className="label-style ">Get Started</label>
@@ -31,7 +48,11 @@ export default function Sidebar() {
             </div>
           </li>
           <li>
-            <div className="flex justify-center">
+            <div
+              className="flex justify-center"
+              onMouseEnter={handleDrawerMenuHover}
+              onMouseLeave={handleDrawerMenuClose}
+            >
               <Link href="/develop" className="sidebar-link group">
                 <CodeIcon className="icon-style group-hover:bg-opacity-70 group-hover:scale-105" />
                 <label className="label-style ">Develop</label>
@@ -39,7 +60,11 @@ export default function Sidebar() {
             </div>
           </li>
           <li>
-            <div className="flex justify-center">
+            <div
+              className="flex justify-center"
+              onMouseEnter={handleDrawerMenuHover}
+              onMouseLeave={handleDrawerMenuClose}
+            >
               <Link href="/foundations" className="sidebar-link group">
                 <BookOutlinedIcon className="icon-style group-hover:bg-opacity-70 group-hover:scale-105" />
                 <label className="label-style">Foundations</label>
@@ -47,7 +72,11 @@ export default function Sidebar() {
             </div>
           </li>
           <li>
-            <div className="flex justify-center">
+            <div
+              className="flex justify-center"
+              onMouseEnter={handleDrawerMenuHover}
+              onMouseLeave={handleDrawerMenuClose}
+            >
               <Link href="/styles" className="sidebar-link group">
                 <PaletteOutlinedIcon className="icon-style group-hover:bg-opacity-70 group-hover:scale-105" />
                 <label className="label-style">Styles</label>
@@ -56,6 +85,18 @@ export default function Sidebar() {
           </li>
         </ul>
       </div>
+
+      {isDrawerOpen && (
+        <div
+          className="fixed ml-[88px] w-[229px] top-0 right-0 bottom-0 left-0 
+          border-l-[1px] border-solid border-[#444746]
+
+          bg-gray-200 dark:bg-gray-800 
+          transition-all rounded-r-2xl"
+          onMouseEnter={handleDrawerMenuHover}
+          onMouseLeave={handleDrawerMenuClose}
+        ></div>
+      )}
 
       <div className="mb-4 flex justify-center">
         <DarkModeToggleButton />
