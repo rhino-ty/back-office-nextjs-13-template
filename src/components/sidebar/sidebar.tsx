c774338875e4e02eabedacbaa4966e55d276f962
Rhino-ty <okko8522@gmail.com>
2023-06-14 12:10:12 +0900

    design: Drawer 메뉴 CSS 적용

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -22,85 +22,82 @@ export default function Sidebar() {
   };
 
   return (
-    <nav className="flex flex-col justify-between h-screen bg-[#f3f6fc] dark:bg-[#2d2f31] fixed">
-      <div className="flex flex-col w-[88px] mt-5">
-        <ul>
-          <li>
-            <div
-              className="flex justify-center"
-              onMouseEnter={handleDrawerMenuClose}
-            >
-              <Link href="/" className="sidebar-link group">
-                <HomeRoundedIcon className="icon-style group-hover:bg-opacity-70 group-hover:scale-105" />
-                <label className="label-style ">Home</label>
-              </Link>
-            </div>
-          </li>
-          <li>
-            <div
-              className="flex justify-center"
-              onMouseEnter={handleDrawerMenuClose}
-            >
-              <Link href="/get-started" className="sidebar-link group">
-                <AppsRoundedIcon className="icon-style group-hover:bg-opacity-70 group-hover:scale-105" />
-                <label className="label-style ">Get Started</label>
-              </Link>
-            </div>
-          </li>
-          <li>
-            <div
-              className="flex justify-center"
-              onMouseEnter={handleDrawerMenuHover}
-              onMouseLeave={handleDrawerMenuClose}
-            >
-              <Link href="/develop" className="sidebar-link group">
-                <CodeIcon className="icon-style group-hover:bg-opacity-70 group-hover:scale-105" />
-                <label className="label-style ">Develop</label>
-              </Link>
-            </div>
-          </li>
-          <li>
-            <div
-              className="flex justify-center"
-              onMouseEnter={handleDrawerMenuHover}
-              onMouseLeave={handleDrawerMenuClose}
-            >
-              <Link href="/foundations" className="sidebar-link group">
-                <BookOutlinedIcon className="icon-style group-hover:bg-opacity-70 group-hover:scale-105" />
-                <label className="label-style">Foundations</label>
-              </Link>
-            </div>
-          </li>
-          <li>
-            <div
-              className="flex justify-center"
-              onMouseEnter={handleDrawerMenuHover}
-              onMouseLeave={handleDrawerMenuClose}
-            >
-              <Link href="/styles" className="sidebar-link group">
-                <PaletteOutlinedIcon className="icon-style group-hover:bg-opacity-70 group-hover:scale-105" />
-                <label className="label-style">Styles</label>
-              </Link>
-            </div>
-          </li>
-        </ul>
-      </div>
+    <>
+      <nav className="flex flex-col justify-between h-screen bg-[#f3f6fc] dark:bg-[#2d2f31] fixed z-[2]">
+        <div className="flex flex-col w-[88px] mt-5 ">
+          <ul>
+            <li>
+              <div
+                className="flex justify-center"
+                onMouseEnter={handleDrawerMenuClose}
+              >
+                <Link href="/" className="sidebar-link group">
+                  <HomeRoundedIcon className="icon-style group-hover:bg-opacity-70 group-hover:scale-105" />
+                  <label className="label-style ">Home</label>
+                </Link>
+              </div>
+            </li>
+            <li>
+              <div
+                className="flex justify-center"
+                onMouseEnter={handleDrawerMenuClose}
+              >
+                <Link href="/get-started" className="sidebar-link group">
+                  <AppsRoundedIcon className="icon-style group-hover:bg-opacity-70 group-hover:scale-105" />
+                  <label className="label-style ">Get Started</label>
+                </Link>
+              </div>
+            </li>
+            <li>
+              <div
+                className="flex justify-center"
+                onMouseEnter={handleDrawerMenuHover}
+                onMouseLeave={handleDrawerMenuClose}
+              >
+                <Link href="/develop" className="sidebar-link group">
+                  <CodeIcon className="icon-style group-hover:bg-opacity-70 group-hover:scale-105" />
+                  <label className="label-style ">Develop</label>
+                </Link>
+              </div>
+            </li>
+            <li>
+              <div
+                className="flex justify-center"
+                onMouseEnter={handleDrawerMenuHover}
+                onMouseLeave={handleDrawerMenuClose}
+              >
+                <Link href="/foundations" className="sidebar-link group">
+                  <BookOutlinedIcon className="icon-style group-hover:bg-opacity-70 group-hover:scale-105" />
+                  <label className="label-style">Foundations</label>
+                </Link>
+              </div>
+            </li>
+            <li>
+              <div
+                className="flex justify-center"
+                onMouseEnter={handleDrawerMenuHover}
+                onMouseLeave={handleDrawerMenuClose}
+              >
+                <Link href="/styles" className="sidebar-link group">
+                  <PaletteOutlinedIcon className="icon-style group-hover:bg-opacity-70 group-hover:scale-105" />
+                  <label className="label-style">Styles</label>
+                </Link>
+              </div>
+            </li>
+          </ul>
+        </div>
 
-      {isDrawerOpen && (
-        <div
-          className="fixed ml-[88px] w-[229px] top-0 right-0 bottom-0 left-0 
-          border-l-[1px] border-solid border-[#444746]
+        <div className="mb-4 flex justify-center">
+          <DarkModeToggleButton />
+        </div>
+      </nav>
 
-          bg-gray-200 dark:bg-gray-800 
-          transition-all rounded-r-2xl"
-          onMouseEnter={handleDrawerMenuHover}
-          onMouseLeave={handleDrawerMenuClose}
-        ></div>
-      )}
-
-      <div className="mb-4 flex justify-center">
-        <DarkModeToggleButton />
-      </div>
-    </nav>
+      <div
+        className={`drawer-menu 
+          ${isDrawerOpen ? "animation-slide-in" : "animation-slide-out"}`}
+        onMouseEnter={handleDrawerMenuHover}
+        onMouseLeave={handleDrawerMenuClose}
+      ></div>
+    </>
   );
 }
