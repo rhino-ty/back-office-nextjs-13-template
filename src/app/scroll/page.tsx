cf377492ca12cd5c0bc6d708e9ee9f64f934498c
Rhino-ty <okko8522@gmail.com>
2023-06-15 18:12:39 +0900

    scroll 가능하게 세팅

--- src/app/scroll/page.tsx
+++ src/app/scroll/page.tsx
@@ -1,3 +1,56 @@
 export default function scroll() {
-  return <div></div>;
+  return (
+    <>
+      <div className='mb-10'>1</div>
+      <div className='mb-10'>2</div>
+      <div className='mb-10'>3</div>
+      <div className='mb-10'>4</div>
+      <div className='mb-10'>5</div>
+      <div className='mb-10'>6</div>
+      <div className='mb-10'>7</div>
+      <div className='mb-10'>8</div>
+      <div className='mb-10'>9</div>
+      <div className='mb-10'>10</div>
+      <div className='mb-10'>11</div>
+      <div className='mb-10'>12</div>
+      <div className='mb-10'>13</div>
+      <div className='mb-10'>14</div>
+      <div className='mb-10'>15</div>
+      <div className='mb-10'>16</div>
+      <div className='mb-10'>17</div>
+      <div className='mb-10'>18</div>
+      <div className='mb-10'>19</div>
+      <div className='mb-10'>20</div>
+      <div className='mb-10'>21</div>
+      <div className='mb-10'>22</div>
+      <div className='mb-10'>23</div>
+      <div className='mb-10'>24</div>
+      <div className='mb-10'>25</div>
+      <div className='mb-10'>26</div>
+      <div className='mb-10'>27</div>
+      <div className='mb-10'>28</div>
+      <div className='mb-10'>29</div>
+      <div className='mb-10'>30</div>
+      <div className='mb-10'>31</div>
+      <div className='mb-10'>32</div>
+      <div className='mb-10'>33</div>
+      <div className='mb-10'>34</div>
+      <div className='mb-10'>35</div>
+      <div className='mb-10'>36</div>
+      <div className='mb-10'>37</div>
+      <div className='mb-10'>38</div>
+      <div className='mb-10'>39</div>
+      <div className='mb-10'>40</div>
+      <div className='mb-10'>41</div>
+      <div className='mb-10'>42</div>
+      <div className='mb-10'>43</div>
+      <div className='mb-10'>44</div>
+      <div className='mb-10'>45</div>
+      <div className='mb-10'>46</div>
+      <div className='mb-10'>47</div>
+      <div className='mb-10'>48</div>
+      <div className='mb-10'>49</div>
+      <div className='mb-10'>50</div>
+    </>
+  );
 }
