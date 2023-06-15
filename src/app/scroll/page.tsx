4dd34e3837f543d1c07972289f5cd33fdc34c928
Rhino-ty <okko8522@gmail.com>
2023-06-15 22:05:09 +0900

    refactor: 고차함수로 간결하게 리팩토링

--- src/app/scroll/page.tsx
+++ src/app/scroll/page.tsx
@@ -1,56 +1,11 @@
-export default function scroll() {
-  return (
-    <>
-      <div className='mb-10 flex justify-center'>1</div>
-      <div className='mb-10 flex justify-center'>2</div>
-      <div className='mb-10 flex justify-center'>3</div>
-      <div className='mb-10 flex justify-center'>4</div>
-      <div className='mb-10 flex justify-center'>5</div>
-      <div className='mb-10 flex justify-center'>6</div>
-      <div className='mb-10 flex justify-center'>7</div>
-      <div className='mb-10 flex justify-center'>8</div>
-      <div className='mb-10 flex justify-center'>9</div>
-      <div className='mb-10 flex justify-center'>10</div>
-      <div className='mb-10 flex justify-center'>11</div>
-      <div className='mb-10 flex justify-center'>12</div>
-      <div className='mb-10 flex justify-center'>13</div>
-      <div className='mb-10 flex justify-center'>14</div>
-      <div className='mb-10 flex justify-center'>15</div>
-      <div className='mb-10 flex justify-center'>16</div>
-      <div className='mb-10 flex justify-center'>17</div>
-      <div className='mb-10 flex justify-center'>18</div>
-      <div className='mb-10 flex justify-center'>19</div>
-      <div className='mb-10 flex justify-center'>20</div>
-      <div className='mb-10 flex justify-center'>21</div>
-      <div className='mb-10 flex justify-center'>22</div>
-      <div className='mb-10 flex justify-center'>23</div>
-      <div className='mb-10 flex justify-center'>24</div>
-      <div className='mb-10 flex justify-center'>25</div>
-      <div className='mb-10 flex justify-center'>26</div>
-      <div className='mb-10 flex justify-center'>27</div>
-      <div className='mb-10 flex justify-center'>28</div>
-      <div className='mb-10 flex justify-center'>29</div>
-      <div className='mb-10 flex justify-center'>30</div>
-      <div className='mb-10 flex justify-center'>31</div>
-      <div className='mb-10 flex justify-center'>32</div>
-      <div className='mb-10 flex justify-center'>33</div>
-      <div className='mb-10 flex justify-center'>34</div>
-      <div className='mb-10 flex justify-center'>35</div>
-      <div className='mb-10 flex justify-center'>36</div>
-      <div className='mb-10 flex justify-center'>37</div>
-      <div className='mb-10 flex justify-center'>38</div>
-      <div className='mb-10 flex justify-center'>39</div>
-      <div className='mb-10 flex justify-center'>40</div>
-      <div className='mb-10 flex justify-center'>41</div>
-      <div className='mb-10 flex justify-center'>42</div>
-      <div className='mb-10 flex justify-center'>43</div>
-      <div className='mb-10 flex justify-center'>44</div>
-      <div className='mb-10 flex justify-center'>45</div>
-      <div className='mb-10 flex justify-center'>46</div>
-      <div className='mb-10 flex justify-center'>47</div>
-      <div className='mb-10 flex justify-center'>48</div>
-      <div className='mb-10 flex justify-center'>49</div>
-      <div className='mb-10 flex justify-center'>50</div>
-    </>
-  );
+export default function Scroll() {
+  const renderItems = () => {
+    return Array.from({ length: 50 }, (_, index) => (
+      <div key={index} className='mb-10 flex justify-center'>
+        {index + 1}
+      </div>
+    ));
+  };
+
+  return <>{renderItems()}</>;
 }
