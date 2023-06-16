538e04d5903d8ef3756d8fb9c40f950313d6c4f9
Rhino-ty <okko8522@gmail.com>
2023-06-16 15:55:44 +0900

    refactor: 다크 라이트 토글 버튼 수정

--- src/components/darkModeToggleButton.tsx
+++ src/components/darkModeToggleButton.tsx
@@ -1,19 +1,19 @@
-import DarkModeIcon from "@mui/icons-material/DarkMode";
-import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
-import LightModeIcon from "@mui/icons-material/LightMode";
-import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
-import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
 
 export default function DarkModeToggleButton() {
   const { theme, setTheme } = useTheme();
   const [hovered, setHovered] = useState(false);
   const [color, setColor] = useState(
-    "dark" ? "text-gray-300" : "text-gray-800"
+    'dark' ? 'text-gray-300' : 'text-gray-800'
   );
 
   useEffect(() => {
-    setColor(theme === "dark" ? "text-gray-300" : "text-gray-800");
+    setColor(theme === 'dark' ? 'text-gray-300' : 'text-gray-800');
   }, [theme]);
 
   const handleMouseEnter = () => {
@@ -25,34 +25,27 @@ export default function DarkModeToggleButton() {
   };
 
   return (
-    <div className='inline-flex items-center w-12 h-12 focus:outline-none rounded text-base my-4'>
+    <div className='focus:outline-none text-base my-4 '>
       <button
-        className='rounded-full border-[1px] border-solid border-gray-800 p-3 border-opacity-60 
+        className='rounded-full border border-solid border-gray-800 p-3 border-opacity-60 
         dark:border-gray-200 dark:border-opacity-50'
         type='button'
         onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
       >
-        {/* 라이트모드 */}
-        {hovered ? (
-          <LightModeIcon
-            className={`${color} visible dark:invisible dark:w-0 dark:h-0 ease-in-out duration-100`}
-          />
+        {theme === 'dark' ? (
+          // 다크모드
+          hovered ? (
+            <DarkModeIcon className={color} />
+          ) : (
+            <DarkModeOutlinedIcon className={color} />
+          )
+        ) : // 라이트모드
+        hovered ? (
+          <LightModeIcon className={color} />
         ) : (
-          <LightModeOutlinedIcon
-            className={`${color} visible dark:invisible dark:w-0 dark:h-0 ease-in-out duration-100`}
-          />
-        )}
-        {/* 다크모드 */}
-        {hovered ? (
-          <DarkModeIcon
-            className={`${color} invisible dark:visible dark:w-6 dark:h-6 w-0 h-0 ease-in-out duration-100`}
-          />
-        ) : (
-          <DarkModeOutlinedIcon
-            className={`${color} invisible dark:visible dark:w-6 dark:h-6 w-0 h-0 ease-in-out duration-100`}
-          />
+          <LightModeOutlinedIcon className={color} />
         )}
       </button>
     </div>
