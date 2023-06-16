b4ed8dd52248a709a4b8716a53050b6fd603adba
Rhino-ty <okko8522@gmail.com>
2023-06-16 11:22:50 +0900

    feat: 팝업-모달 컴포넌트 구현
    모달을 컴포넌트화 시켰지만 일단 직관적으로 보기 위해 page에 전부 넣었음

---
+++ src/app/popup/page.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import { useState } from 'react';
+
+export default function Popup() {
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => setShowModal(true);
+  const closeModal = () => setShowModal(false);
+
+  return (
+    <div className='flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0'>
+      <button
+        onClick={openModal}
+        className='text-xl w-40 text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg px-5 py-2.5 text-center 
+                   dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'
+      >
+        Popup
+      </button>
+      {showModal && (
+        <>
+          <div className='justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none'>
+            <div className='relative w-80 my-6 mx-auto max-w-3xl'>
+              {/*content*/}
+              <div className='border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none'>
+                {/*header*/}
+                <div className='flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t'>
+                  <h3 className='text-3xl font-semibold'>Modal Title</h3>
+                </div>
+                {/*body*/}
+                <div className='relative p-6 flex-auto'>
+                  <p className='my-4 text-slate-500 text-lg leading-relaxed'>
+                    HI! I&apos;m modal
+                  </p>
+                  <input
+                    type='text'
+                    name='text'
+                    id='text'
+                    className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-sidebar-bg-dark dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                  />
+                </div>
+                {/*footer*/}
+                <div className='flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b'>
+                  <button
+                    className='text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
+                    type='button'
+                    onClick={closeModal}
+                  >
+                    Close
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='opacity-25 fixed inset-0 z-40 bg-black'></div>
+        </>
+      )}
+    </div>
+  );
+}
