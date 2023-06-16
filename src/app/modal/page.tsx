806fd43d79018051db80f91e0acdff9db84f7656
Rhino-ty <okko8522@gmail.com>
2023-06-16 17:14:59 +0900

    design: 토스트, 모달 다크모드 추가

--- src/app/modal/page.tsx
+++ src/app/modal/page.tsx
@@ -32,16 +32,18 @@ export default function Popup() {
       {showModal && (
         <>
           <div className='justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none'>
-            <div className='relative w-80 my-6 mx-auto max-w-3xl'>
+            <div className='relative w-80 my-6 mx-auto max-w-3xl dark:bg-gray-800'>
               {/*content*/}
-              <div className='border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none'>
+              <div className='border-0 rounded-lg shadow-lg relative flex flex-col w-full outline-none focus:outline-none dark:border-gray-600'>
                 {/*header*/}
-                <div className='flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t'>
-                  <h3 className='text-3xl font-semibold'>Modal Title</h3>
+                <div className='flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t dark:border-gray-600'>
+                  <h3 className='text-3xl font-semibold dark:text-white'>
+                    Modal Title
+                  </h3>
                 </div>
                 {/*body*/}
                 <div className='relative p-6 flex-auto'>
-                  <p className='my-4 text-slate-500 text-lg leading-relaxed'>
+                  <p className='my-4 text-slate-500 text-lg leading-relaxed dark:text-gray-300'>
                     HI! I&apos;m modal
                   </p>
                   <input
@@ -50,11 +52,11 @@ export default function Popup() {
                     id='text'
                     value={inputValue}
                     onChange={inputChangeHandler}
-                    className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-sidebar-bg-dark dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                    className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-window-bg-dark dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
                   />
                 </div>
                 {/*footer*/}
-                <div className='flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b'>
+                <div className='flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b dark:border-gray-600'>
                   <button
                     className='text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
                     type='button'
@@ -66,7 +68,7 @@ export default function Popup() {
               </div>
             </div>
           </div>
-          <div className='opacity-25 fixed inset-0 z-40 bg-black'></div>
+          <div className='opacity-25 fixed inset-0 z-40 bg-black dark:bg-gray-200'></div>
         </>
       )}
     </div>
