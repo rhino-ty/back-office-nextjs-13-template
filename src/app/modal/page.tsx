a960c140006e90df0c26552907a0bf8d10ed5898
Rhino-ty <okko8522@gmail.com>
2023-06-19 12:06:30 +0900

    feat: mui 모달 사용

--- src/app/modal/page.tsx
+++ src/app/modal/page.tsx
@@ -1,76 +1,82 @@
-'use client';
-import { useDispatch, useSelector } from 'react-redux';
-import { RootState } from '../../store';
-import { closeModal, openModal, setInputValue } from '../../store/modal';
+'use client'
+// mui 모달 만들기
+// TODO: MUI 사용하여 모달에 모달을 넣을 수 있는지 확인
 
-export default function Popup() {
-  const dispatch = useDispatch();
-  const showModal = useSelector((state: RootState) => state.showModal);
-  const inputValue = useSelector((state: RootState) => state.inputValue);
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Button from '@mui/material/Button';
 
-  const openModalHandler = () => {
-    dispatch(openModal());
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  pt: 2,
+  px: 4,
+  pb: 3,
+};
+
+function ChildModal() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => {
+    setOpen(true);
   };
-
-  const closeModalHandler = () => {
-    dispatch(closeModal());
-  };
-
-  const inputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    dispatch(setInputValue(e.target.value));
+  const handleClose = () => {
+    setOpen(false);
   };
 
   return (
-    <div className='flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0'>
-      <button
-        onClick={openModalHandler}
-        className='text-xl w-40 text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg px-5 py-2.5 text-center 
-                   dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'
+    <React.Fragment>
+      <Button onClick={handleOpen}>Open Child Modal</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="child-modal-title"
+        aria-describedby="child-modal-description"
       >
-        Modal
-      </button>
-      {showModal && (
-        <>
-          <div className='justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none'>
-            <div className='relative w-80 my-6 mx-auto max-w-3xl bg-white dark:bg-window-bg-dark'>
-              {/*content*/}
-              <div className='border-0 rounded-lg shadow-lg relative flex flex-col w-full outline-none focus:outline-none dark:border-gray-600'>
-                {/*header*/}
-                <div className='flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t dark:border-gray-600'>
-                  <h3 className='text-3xl font-semibold dark:text-white'>
-                    Modal Title
-                  </h3>
-                </div>
-                {/*body*/}
-                <div className='relative p-6 flex-auto'>
-                  <p className='my-4 text-slate-500 text-lg leading-relaxed dark:text-gray-100'>
-                    HI! I&apos;m modal
-                  </p>
-                  <input
-                    type='text'
-                    name='text'
-                    id='text'
-                    value={inputValue}
-                    onChange={inputChangeHandler}
-                    className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-window-bg-dark dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-                  />
-                </div>
-                {/*footer*/}
-                <div className='flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b dark:border-gray-600'>
-                  <button
-                    className='text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
-                    type='button'
-                    onClick={closeModalHandler}
-                  >
-                    Close
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className='opacity-25 fixed inset-0 z-40 bg-black dark:bg-gray-200'></div>
-        </>
-      )}
+        <Box sx={{ ...style, width: 200 }}>
+          <h2 id="child-modal-title">Text in a child modal</h2>
+          <p id="child-modal-description">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          </p>
+          <Button onClick={handleClose}>Close Child Modal</Button>
+        </Box>
+      </Modal>
+    </React.Fragment>
+  );
+}
+
+export default function NestedModal() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button onClick={handleOpen}>Open modal</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="parent-modal-title"
+        aria-describedby="parent-modal-description"
+      >
+        <Box sx={{ ...style, width: 400 }}>
+          <h2 id="parent-modal-title">Text in a modal</h2>
+          <p id="parent-modal-description">
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </p>
+          <ChildModal />
+        </Box>
+      </Modal>
     </div>
   );
 }
---
+++ src/app/modal/page_custom_redux.tsx
@@ -0,0 +1,76 @@
+'use client';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../store';
+import { closeModal, openModal, setInputValue } from '../../store/modal';
+
+export default function Popup() {
+  const dispatch = useDispatch();
+  const showModal = useSelector((state: RootState) => state.showModal);
+  const inputValue = useSelector((state: RootState) => state.inputValue);
+
+  const openModalHandler = () => {
+    dispatch(openModal());
+  };
+
+  const closeModalHandler = () => {
+    dispatch(closeModal());
+  };
+
+  const inputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(setInputValue(e.target.value));
+  };
+
+  return (
+    <div className='flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0'>
+      <button
+        onClick={()=>{openModalHandler()}}
+        className='text-xl w-40 text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg px-5 py-2.5 text-center 
+                   dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'
+      >
+        Modal
+      </button>
+      {showModal && (
+        <>
+          <div className='justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none'>
+            <div className='relative w-80 my-6 mx-auto max-w-3xl bg-white dark:bg-window-bg-dark'>
+              {/*content*/}
+              <div className='border-0 rounded-lg shadow-lg relative flex flex-col w-full outline-none focus:outline-none dark:border-gray-600'>
+                {/*header*/}
+                <div className='flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t dark:border-gray-600'>
+                  <h3 className='text-3xl font-semibold dark:text-white'>
+                    Modal Title
+                  </h3>
+                </div>
+                {/*body*/}
+                <div className='relative p-6 flex-auto'>
+                  <p className='my-4 text-slate-500 text-lg leading-relaxed dark:text-gray-100'>
+                    HI! I&apos;m modal
+                  </p>
+                  <input
+                    type='text'
+                    name='text'
+                    id='text'
+                    value={inputValue}
+                    onChange={(e)=>{inputChangeHandler(e)}}
+                    className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-window-bg-dark dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                  />
+                </div>
+                {/*footer*/}
+                <div className='flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b dark:border-gray-600'>
+                  <button
+                    className='text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
+                    type='button'
+                    onClick={()=>{closeModalHandler()}}
+                  >
+                    Close
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='opacity-25 fixed inset-0 z-40 bg-black dark:bg-gray-200'></div>
+        </>
+      )}
+    </div>
+  );
+}
