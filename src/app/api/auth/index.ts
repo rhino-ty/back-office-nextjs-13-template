14006a0ebbb6dd89fb0d0f3de4b4e0faa527d2c7
Rhino-ty <okko8522@gmail.com>
2023-06-19 15:46:11 +0900

    feat: 구현된 로컬스토리지 get, set 함수 적용 및 파일 명 수정

--- src/app/api/auth/index.ts
+++ src/app/api/auth/index.ts
@@ -1,6 +1,6 @@
 'use client';
-
-import instance from '../axiosModule';
+import { saveAccessTokenToLocalStorage } from '@/utils/acces_token_handler';
+import instance from '../axios_module';
 
 export const handleLoginSubmit = async (email: string, password: string) => {
   try {
@@ -11,7 +11,7 @@ export const handleLoginSubmit = async (email: string, password: string) => {
 
     // 로그인 요청 성공
     console.log(response.data);
-    localStorage.setItem('access_token', response.data.token);
+    saveAccessTokenToLocalStorage(response.data.token);
   } catch (error) {
     // 로그인 요청 실패
     console.error(error);
--- src/app/api/axios_module.ts
+++ src/app/api/axios_module.ts
@@ -1,3 +1,4 @@
+import { getAccessTokenFromLocalStorage } from '@/utils/acces_token_handler';
 import axios from 'axios';
 
 const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL;
@@ -5,7 +6,7 @@ const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL;
 const instance = axios.create({
   baseURL: SERVER_URL,
   headers: {
-    Authorization: `Bearer ${localStorage.getItem('access_token')}}`,
+    Authorization: `Bearer ${getAccessTokenFromLocalStorage}}`,
   },
 });
 
