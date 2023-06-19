faf45e73db897770c251a490599c6a0c44928b60
Rhino-ty <okko8522@gmail.com>
2023-06-19 15:12:20 +0900

    feat: child 모달 디자인 수정 및 grandchild 추가 중첩

--- src/app/modal/page.tsx
+++ src/app/modal/page.tsx
@@ -14,12 +14,42 @@ const style = {
   transform: 'translate(-50%, -50%)',
   width: 400,
   bgcolor: 'background.paper',
-  border: '2px solid #000',
+  borderRadius: 3,
   boxShadow: 24,
   pt: 2,
   px: 4,
   pb: 3,
 };
+function GrandChildModal() {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Fragment>
+      <Button onClick={handleOpen}>Open Grandchild Modal</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby='child-modal-title'
+        aria-describedby='child-modal-description'
+      >
+        <Box sx={{ ...style, width: 350 }}>
+          <h2 className='font-bold text-2xl my-6'>Grandchild modal</h2>
+          <p className='mb-4'>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          </p>
+
+          <Button onClick={handleClose}>Close GrandChild Modal</Button>
+        </Box>
+      </Modal>
+    </Fragment>
+  );
+}
 
 function ChildModal() {
   const [open, setOpen] = useState(false);
@@ -39,10 +69,15 @@ function ChildModal() {
         aria-labelledby='child-modal-title'
         aria-describedby='child-modal-description'
       >
-        <Box sx={{ ...style, width: 200 }}>
-          <h2>Text in a child modal</h2>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-          <Button onClick={handleClose}>Close Child Modal</Button>
+        <Box sx={{ ...style, width: 350 }}>
+          <h2 className='font-bold text-2xl my-6'>Text in a child modal</h2>
+          <p className='mb-4'>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          </p>
+          <div className='flex justify-between'>
+            <GrandChildModal />
+            <Button onClick={handleClose}>Close Child Modal</Button>
+          </div>
         </Box>
       </Modal>
     </Fragment>
@@ -59,7 +94,7 @@ export default function NestedModal() {
   };
 
   return (
-    <div className='flex justify-center items-center'>
+    <div className='min-h-screen flex justify-center items-center'>
       <Button onClick={handleOpen}>Open modal</Button>
       <Modal
         open={open}
@@ -68,8 +103,8 @@ export default function NestedModal() {
         aria-describedby='parent-modal-description'
       >
         <Box sx={{ ...style, width: 400 }}>
-          <h2>Text in a modal</h2>
-          <p>
+          <h2 className='font-bold text-2xl my-6'>Text in a modal</h2>
+          <p className='mb-4'>
             Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
           </p>
           <ChildModal />
