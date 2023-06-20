7c0ad08c4f605b482c0e3d34a987651ede83c53f
Rhino-ty <okko8522@gmail.com>
2023-06-20 13:44:40 +0900

    feat: Instance 개념을 뺀 basic fetch API 코드 작성

---
+++ src/app/api/basic_fetch/pagination/index.ts
@@ -0,0 +1,13 @@
+import { SERVER_URL } from '../..';
+
+export const handlePageLoad = async (page: number) => {
+  try {
+    const response = await fetch(`${SERVER_URL}/users?page=${page}`);
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
--- src/app/api/index.ts
+++ src/app/api/index.ts
@@ -1,10 +1,10 @@
 // fetch_instance
-// fetch를 이용해 API 작성을 보다 쉽고, 간편하게 만듦
+// fetch를 이용해 API 작성을 보다 쉽고, 간편하게 만듦. 기본적인
 // url만 있으면 데이터가 return 되게!
 
 import { getAccessTokenFromLocalStorage } from '@/utils/acces_token_handler';
 
-const SERVER_URL: string = process.env.NEXT_PUBLIC_SERVER_URL || '';
+export const SERVER_URL: string = process.env.NEXT_PUBLIC_SERVER_URL || '';
 
 interface FetchOptions extends RequestInit {
   headers?: HeadersInit;
